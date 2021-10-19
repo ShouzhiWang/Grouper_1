5e12255a188ac1abb75159e310422d9a76a2ab26
swang <shouzhiwang@qq.com>
2021-10-19 13:23:20 -0400

    Initial Commit

---
+++ Grouper/GrouperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrouperApp.swift
+//  Grouper
+//
+//  Created by 王首之 on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct GrouperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
