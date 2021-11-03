3bb1de248d38b75b40744ba1da5beb10f28affdf
swang <shouzhiwang@qq.com>
2021-11-03 08:58:48 -0400

    'list of people' improved

--- Grouper/Views/List/Models/ListOfPeople.swift
+++ Grouper/Views/List/Models/ListOfPeople.swift
@@ -8,17 +8,23 @@
 import Foundation
 import SwiftUI
 
-struct ContactList: Hashable, Codable {
+struct ContactList: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var group: String
     var role: String
     var description: String
+    var isFeatured: Bool
     
     
     private var imageName: String
     var image: Image {
-        Image(imageName)
+        if imageName == "Default" {
+            return Image(systemName: "person.circle")
+        }
+        else {
+            return Image(imageName)
+        }
     }
 }
 
--- Grouper/Views/List/View/ListRow.swift
+++ Grouper/Views/List/View/ListRow.swift
@@ -8,15 +8,23 @@
 import SwiftUI
 
 struct ListRow: View {
-    var listofpeople: ListOfPeople
+    var listofpeople: ContactList
     
     var body: some View {
+        HStack{
+            listofpeople.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(listofpeople.name)
+
+                       Spacer()
+        }
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 struct ListRow_Previews: PreviewProvider {
     static var previews: some View {
-        ListRow(listofpeople: ContactList)
+        ListRow(listofpeople: peoples[0])
     }
 }
